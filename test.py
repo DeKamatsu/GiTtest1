9769c89add4ddb0547c650ad1420f778e45911bb
Denis Matveev <denis_matveev@tut.by>
2022-02-26 20:26:22 +0300

    correction in b_2: should - not could

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 # 2-ed file for GiT-testing
 print('Hello world!')
 
-print("\nThis message could be added in 2-nd branch only first")
+print("\nThis message should be added in 2-nd branch only first")
