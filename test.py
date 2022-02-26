f468bb9a2f6f8876e2892ae223680ac59c131513
Denis Matveev <denis_matveev@tut.by>
2022-02-26 20:41:57 +0300

    Final edition of merged branches. But do I need to delete b_1 & b_2?

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 # file for GiT-testing - main
 print('Hello world!')
-=======
-# 2-ed file for GiT-testing
-print('Hello world!')
 
-print("\nThis message should be added in 2-nd branch only first")
+print("\nThis message should be added in 2-nd branch only first. But can be printed in main after marge")
 # Finally I need to change something more to check one option
->>>>>>> b_2
+
